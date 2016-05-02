5095b6e9c757064745e73e6c1c6c0fb15330fbe3
yuta kimura <bumplive@gmail.com>
2016-05-02 16:12:47 +0900

    commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+  Rails.application.routes.draw do
+  devise_for :users
+  root 'tweets#index'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
+end
